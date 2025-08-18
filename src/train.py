f6656d8df5e8435e45330b493e8eb7956c0989ba
KaChunMa <jasparkc@protonmail.com>
2025-08-18 12:52:09 +0100

    ver 0.6

--- src/train.py
+++ src/train.py
@@ -2,7 +2,8 @@ from sklearn.datasets import load_iris #getting the data
 iris = load_iris()
 X = iris.data      # shape (150, 4)
 y = iris.target    # shape (150,)
-#print(iris.feature_names, iris.target_names)
+print(iris.feature_names, iris.target_names)
+
 
 from sklearn.model_selection import train_test_split
 
@@ -10,74 +11,41 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 #split data 80/20 into train/test
 #random_state=42 ensures the shuffle is deterministic for reproducible results
 
+
 from sklearn.tree import DecisionTreeClassifier
 model = DecisionTreeClassifier(random_state=42)
 #random_state=42 ensures reproducible results
 
+
 model.fit(X_train, y_train) #training the model
 
+
 y_pred = model.predict(X_test) #predict with model, then compare the first 5
-#print("Predictions:", y_pred[:5])
-#print("True labels:", y_test[:5])
+
 
 from sklearn.metrics import accuracy_score #show how accurate the model is
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-from sklearn.metrics import confusion_matrix #show the confusion matrix
-cm = confusion_matrix(y_test, y_pred) #> pred, v true
-print("Confusion Matix:\n",cm)
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = {
-    'True (below) & Predictions (right)': ['setosa','versicolor','virginica'],
-    'setosa': [cm[0,0],cm[1,0],cm[2,0]],
-    'versicolor': [cm[0,1],cm[1,1],cm[2,1]],
-    'virginica': [cm[0,2],cm[1,2],cm[2,2]]
-}
-
-df = pd.DataFrame(data)
-#print(df)
-
-# Create a figure and axis
-fig, ax = plt.subplots(figsize=(10, 5))
-
-# Hide axes
-ax.axis('tight')
-ax.axis('off')
-
-# Create the table and add it to the axis
-table = ax.table(cellText=df.values, 
-                 colLabels=df.columns, 
-                 loc='center', 
-                 cellLoc='center',
-                 colColours=['#f2f2f2']*len(df.columns))
-
-# Style the table
-table.auto_set_font_size(True)
-table.scale(1.2, 2)  # Adjust table size (x,y)
-
-#saving the file to outputs
-save_directory = "../outputs"  
-filename = "confusion_matrix.png"
 
 import os
 
-save_directory = "../outputs"  #file path
-filename = "confusion_matrix.png" #file name
+# Creates outputs folder if it doesn't exist
+os.makedirs("../outputs", exist_ok=True)
 
-# Create outputs if it doesn't exist
-os.makedirs(save_directory, exist_ok=True)
 
-# Joining the directory and filename to create the full path
-full_path = os.path.join(save_directory, filename)
+import joblib
 
-# Savivg the figure to the specified path
-plt.savefig(full_path, 
-            bbox_inches='tight',     # Triming whitespace around the table
-            #dpi=300,              # Higher DPI for better quality
-            transparent=False)       # Set to True if you want transparent background
+joblib.dump(model,"../outputs/model.pkl") #outputing the model
 
-plt.close() #close plot
+
+from sklearn.metrics import confusion_matrix #generate the confusion matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import ConfusionMatrixDisplay #display the confusion matrix
+import matplotlib.pyplot as plt
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["setosa","versicolor","virginica"])
+cm_display.plot()
+plt.savefig("../outputs/confusion_matrix.png", dpi=300, bbox_inches='tight') #outputing the matrix
+plt.show() #plt.show clears the figure in some environment some save the figure first
